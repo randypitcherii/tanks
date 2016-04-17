dcdacedaa92a3027412ded95d0eba22a5b1fcf5a
Randy <rpitcher@purdue.edu>
2016-04-17 18:28:15 -0400

    this is awful

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var host = (process.env.VCAP_APP_HOST || 'localhost');
 
 //handle the default request
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/html/index.html');
+	res.sendFile(__dirname + '/public/html/tanks.html');
 });
 
 //establish access to public folder
