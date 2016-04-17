a3bab547d73970a0d3e55c6188e2c6fbb1d5d9d8
Randy <rpitcher@purdue.edu>
2016-04-17 17:49:13 -0400

    Fixed express usage

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //require external libraries
-var app = require('express')();
+var express = require('express')
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
