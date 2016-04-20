5da949f02dbeb823406c6cf60df85795b1cefc54
Randy <rpitcher@purdue.edu>
2016-04-19 20:29:58 -0400

    created profile system

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
-//require external libraries
+//require core external libraries
 var express = require('express')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+//require helper libraries
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+//database config file
+var configDB = require('./config/database.js');
 
 //setup port and host. This allows the same code to be run on bluemix
 //and locally.
 var port = (process.env.VCAP_APP_PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 
-//handle the default request
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/html/tanks.html');
-});
+//setup database
+mongoose.connect(configDB.url);//connect to our DB
 
-//establish access to public folder
+//configure passport
+require('./config/passport')(passport);
+
+//setup express
+app.use(morgan('dev')); //log every request to the console
+app.use(cookieParser()); //read cookies
+app.use(bodyParser()); //read html forms
 app.use(express.static('public'));
+app.set('view engine', 'ejs'); //setup ejs for templating
+
+//setup passport
+app.use(session({ secret: 'RavenclawAllDay'}));//session secret
+app.use(passport.initialize());
+app.use(passport.session()); //persistent login sessions
+app.use(flash()); //use for flash messages
+
+//setup routes
+require('./app/routes.js')(app, passport);
 
 //establish socket listener
 io.on('connection', function(socket) {
@@ -26,6 +52,5 @@ io.on('connection', function(socket) {
 });
 
 //start the server.
-http.listen(port, function() {
-    console.log('listening on ' + host + ':' + port);
-});
+app.listen(port);
+console.log("Listening on port " + port);
